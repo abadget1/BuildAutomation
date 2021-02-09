6597955e3a5fa845ce81797fba4a876544698d22
Aaron Badgett <aaron.badgett@outlook.com>
2021-02-09 08:39:12 -0500

    Updated 'GitBot' & 'jumpstart'

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 function jumpstart() {
-    cd ~/Desktop
-    python3 jumpstart.py $1
-    # mkdir ~/Desktop/Web/BuildAutomation/$1
-    # cd ~/Desktop/Web/BuildAutomation/$1
-    # git init
-    # git remote add origin https://github.com/$GIT_USERNAME/$1.git
-    # git add .
-    # touch READ.md
-    # git commit -m "first commit"
-    # git push -u origin main
-    # code .
+    cd ~
+    python3 $PATH/jumpstart.py $1
+    mkdir ~/Desktop/$1
+    cd ~/Desktop/$1
+    git init
+    git remote add $1 https://github.com/$GIT_USERNAME/$1.git
+    git add .
+    touch READ.md
+    git commit -m "first commit"
+    git push -u $1 master
+    code .
 }

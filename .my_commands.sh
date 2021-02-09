087c34d1d4bfa553836dbd4604a9ec26308c472e
Aaron Badgett <32856818+abadget1@users.noreply.github.com>
2021-02-09 10:04:00 -0500

    Update and rename my_commands.sh to .my_commands.sh

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 function jumpstart() {
     cd ~
-    python3 /Users/Aaron/Desktop/Web/BuildAutomation/$1
+    python3 jumpstart.py $1
     mkdir ~/Desktop/$1
     cd ~/Desktop/$1
     git init
-    git remote add $1 https://github.com/abadget1/$1.git
+    git remote add $1 https://github.com/$GIT_USERNAME/$1.git
     git add .
     touch READ.md
     git commit -m "first commit"
